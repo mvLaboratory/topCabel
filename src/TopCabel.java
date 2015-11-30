cdd28ffcadadf6fba6c323108e7a9858709cfe40
mvLaboratory <mvLabaratory@gmail.com>
2015-12-01 00:40:21 +0200

    0.2

--- src/TopCabel.java
+++ src/TopCabel.java
@@ -8,7 +8,7 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class TopCabel {
-    public static void main(String[] args) throws Exception {
+     public static void main(String[] args) throws Exception {
         String url = "http://topkabel.ru/";
         Connection connection = Jsoup.connect(url);
         Document document = connection.get();
@@ -22,7 +22,7 @@ public class TopCabel {
                 String linkText = link.text();
 
                 if (linkHref.startsWith("https")) {
-                   // System.out.println(linkHref);
+                    // System.out.println(linkHref);
                     connection = Jsoup.connect(linkHref);
                     document = connection.get();
                     readGoods(document, linkText);
@@ -35,9 +35,17 @@ public class TopCabel {
     public static void readGoods(Document document, String category) {
         Elements elements = document.select(".cart-table");
         for (Element element : elements) {
-            System.out.println("cat:" + category);
-            System.out.println("cat:" + category);
-            System.out.println(element);
+            Elements table = element.select("td");
+            for (Element row : table) {
+                if  (row.childNodeSize() == 3)
+                    //System.out.println(row.childNode(1));
+                else if (row.childNodeSize() > 0)
+                    //System.out.println(row.childNode(0));
+            }
         }
+    }
+    
+    public static void parseLink(String link) {
+        System.out.println(link);        
     }
 }
