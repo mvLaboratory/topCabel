e91c0ad960fb99daace86082ca4cc8ea0dd672bc
mvLaboratory <mvLabaratory@gmail.com>
2015-12-02 00:39:58 +0200

    3

--- src/TopCabel.java
+++ src/TopCabel.java
@@ -6,9 +6,10 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import java.io.IOException;
 
 public class TopCabel {
-     public static void main(String[] args) throws Exception {
+        public static void main(String[] args) throws Exception {
         String url = "http://topkabel.ru/";
         Connection connection = Jsoup.connect(url);
         Document document = connection.get();
@@ -35,17 +36,65 @@ public class TopCabel {
     public static void readGoods(Document document, String category) {
         Elements elements = document.select(".cart-table");
         for (Element element : elements) {
+            int index = 0;
             Elements table = element.select("td");
             for (Element row : table) {
                 if  (row.childNodeSize() == 3)
-                    //System.out.println(row.childNode(1));
+                    parseLink(row, index, category);
                 else if (row.childNodeSize() > 0)
-                    //System.out.println(row.childNode(0));
+                    parseLink(row, index, category);
+                index = (++index) % 7;
             }
         }
     }
-    
-    public static void parseLink(String link) {
-        System.out.println(link);        
+
+    public static void parseLink(Element link, int index, String cat) {
+        if (index == 0) {
+            System.out.println(cat);
+            readLink(link); }
+        else if (index == 1) {
+            readArt(link);
+        }
+        else if (index == 2)
+            return;
+        else if (index == 3)
+            return;
+        else if (index == 4)
+            return;
+        else if (index == 5)
+            readDeskription(link);
+        else if (index == 6)
+            return;
+        else
+            System.out.println(index + ":" + " " + link);
+    }
+
+    public static void readLink(Element link) {
+        String txt = link.text();
+        String href = link.select("a").get(0).attr("href");
+        readFullDeskription(href);
+
+        System.out.println(txt + ": " + href);
+    }
+
+    public static void readArt(Element link) {
+        String art = link.text();
+        System.out.println("art: " + art);
+    }
+
+    public static void readDeskription(Element link) {
+        String deskription = link.text();
+        System.out.println("desk:" + deskription);
+    }
+
+    public static void readFullDeskription(String link) {
+        Connection connection = Jsoup.connect(link);
+        try {
+            Document document = connection.get();
+            Elements desk = document.select(".product-tabs-container");
+            String fullDesk = desk.select("p").get(0).text();
+            System.out.println(fullDesk);
+        }
+        catch (IOException e) {}
     }
 }
